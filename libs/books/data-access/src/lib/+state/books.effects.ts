59f105fcb4050796f1c87a0da0bb262061f1bdcd
Ramandeep Guliani <ramandeep.guliani@ge.com>
2020-07-11 23:12:17 -0700

    feat instant search

--- libs/books/data-access/src/lib/+state/books.effects.ts
+++ libs/books/data-access/src/lib/+state/books.effects.ts
@@ -3,7 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { fetch } from '@nrwl/angular';
 import * as BooksActions from './books.actions';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Book } from '@tmo/shared/models';
 
 @Injectable()
@@ -11,16 +11,19 @@ export class BooksEffects {
   searchBooks$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BooksActions.searchBooks),
+      map(action => action.term),
+      debounceTime(500),
+      distinctUntilChanged(),
       fetch({
-        run: action => {
+        run: (term: any) => {
           return this.http
-            .get<Book[]>(`/api/books/search?q=${action.term}`)
+            .get<Book[]>(`/api/books/search?q=${term}`)
             .pipe(
               map(data => BooksActions.searchBooksSuccess({ books: data }))
             );
         },
 
-        onError: (action, error) => {
+        onError: (_, error) => {
           console.error('Error', error);
           return BooksActions.searchBooksFailure({ error });
         }
